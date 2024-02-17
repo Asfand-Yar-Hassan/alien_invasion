2b3dfc2c1183dbb53af2da2e423b54209e91bfee
Asfand Yar Hassan <77690107+Asfand-Yar-Hassan@users.noreply.github.com>
2024-02-17 01:23:48 -0500

    Flag to check if the game is active

--- alien_invasion.py
+++ alien_invasion.py
@@ -27,15 +27,17 @@ class AlienInvasion:
   self.aliens = pygame.sprite.Group()
   
   self._create_fleet()
+  self.game_active = True
   
   
  def run_game(self):
   """Start the main loop for the game"""
   while True:
-   self.check_events() 
-   self.ship.update()
-   self._update_bullets()
-   self._update_aliens()
+   self.check_events()
+   if self.game_active:
+     self.ship.update()
+     self._update_bullets()
+     self._update_aliens()
    self.update_screen()
    self.clock.tick(60)
   
@@ -120,21 +122,31 @@ class AlienInvasion:
    self._check_fleet_edges()
    self.aliens.update()
    self._check_ship_collision()
+   self._check_aliens_bottom()
    
  def _check_ship_collision(self):
      if pygame.sprite.spritecollideany(self.ship, self.aliens):
        self.ship_hit()
+      
+ def _check_aliens_bottom(self):
+   for alien in self.aliens.sprites():
+     if alien.rect.bottom >= self.settings.screen_height:
+       self.ship_hit()
+       break     
        
  def ship_hit(self):
-   self.stats.ships_left -= 1
+   if self.stats.ships_left > 0:
+    self.stats.ships_left -= 1
    
-   # Get rid off bullets and aliens once the ship is hit
-   self.bullets.empty()
-   self.aliens.empty()
-  # Create a new ship and center it
-   self._create_fleet()
-   self.ship.center_ship()
-   sleep(0.5) 
+    # Get rid off bullets and aliens once the ship is hit
+    self.bullets.empty()
+    self.aliens.empty()
+    # Create a new ship and center it
+    self._create_fleet()
+    self.ship.center_ship()
+    sleep(0.5)
+   else:
+     self.game_active = False 
     
  def _check_fleet_edges(self):
    for alien in self.aliens.sprites():
